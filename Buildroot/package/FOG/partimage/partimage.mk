4554ed746390f3cabeb22481ddc26a4c43f67bd5
cedric-d <cedric-d@users.noreply.github.com>
2019-04-22 08:31:49 +0200

    Use the version variable within the URL

--- Buildroot/package/FOG/partimage/partimage.mk
+++ Buildroot/package/FOG/partimage/partimage.mk
@@ -6,7 +6,7 @@
 
 PARTIMAGE_VERSION = 0.6.9
 PARTIMAGE_SOURCE = partimage-$(PARTIMAGE_VERSION).tar.bz2
-PARTIMAGE_SITE = http://downloads.sourceforge.net/project/partimage/stable/0.6.9
+PARTIMAGE_SITE = http://downloads.sourceforge.net/project/partimage/stable/$(PARTIMAGE_VERSION)
 PARTIMAGE_DEPENDENCIES = e2fsprogs
 PARTIMAGE_INSTALL_STAGING = YES
 PARTIMAGE_CONF_OPTS = ac_cv_func_setpgrp_void=yes --disable-ssl
